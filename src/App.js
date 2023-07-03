a4277b93f72bbb8b19a541cdf9ad62c4b2e29fdf
premnath pichaimuthu <premwebdev+dev@gmail.com>
2023-07-03 14:17:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+const initialValue = [
+  {
+    id: 1,
+    name: "Chicken Burger",
+    quantity: 40,
+    desc: "Fried chicken burger - lettuce, tomato, cheese and mayonnaise",
+    price: "24",
+    image: "cb.jpg"
+  },
+  {
+    id: 2,
+    name: "Veg Burger",
+    quantity: 30,
+    desc: "Plant based burger - lettuce, tomato, vegan cheese and mayonnaise",
+     price: "22",
+    image: "vb.jpg"
+  },
+  {
+    id: 3,
+    name: "Chips",
+    quantity: 50,
+    desc: "Potato chips fried to perfection",
+     price: "10",
+    image: "chips.jpg"
+  },
+  {
+    id: 4,
+    name: "Ice Creams",
+    quantity: 30,
+    desc: "Ice Cream - Vannila ouble scoop",
+     price: "7",
+    image: "ic.jpg"
+  },
+];
 function App() {
+  const [menuItems, setMenuItems] = useState(initialValue);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Food online shop</header>
+      <h4 className="subtitle">Menu Availability</h4>
+      <ul className="ulapp">
+        {
+          menuItems.map((item)=><li className="liapp" key={item.id}>
+            {item.name} - {item.quantity}
+          </li>)
+        }
+      </ul>
     </div>
   );
 }
